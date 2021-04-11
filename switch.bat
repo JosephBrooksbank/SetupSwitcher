4f78a6dee9e260d89813c8860751cbd43d4dc24e
Joseph Brooksbank <Joseph.Brooksbank@du.edu>
2021-04-11 14:54:58 -0600

    Initial Commit

---
+++ switch.bat
@@ -0,0 +1,3 @@
+ControlMyMonitor.exe /SetValue "\\.\DISPLAY2\Monitor0" 60 17
+ControlMyMonitor.exe /SetValue "\\.\DISPLAY3\Monitor0" 60 15
+ControlMyMonitor.exe /SetValue "\\.\DISPLAY1\Monitor0" 60 3
